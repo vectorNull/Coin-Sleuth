2b57c08c084873379b770347a0554f882f810583
Abdul Ghaffar Rodriguez <abdul_ghaffar@prominentcode.com>
2022-03-06 17:10:13 -0800

    Create context

---
+++ src/CryptoContext.jsx
@@ -0,0 +1,27 @@
+import React, { useContext, useEffect, useState } from "react";
+import { createContext } from "react";
+
+const Crypto = createContext();
+
+const CryptoContext = ({ children }) => {
+	const [currency, setCurrency] = useState("USD");
+	const [symbol, setSymbol] = useState("$");
+
+	useEffect(() => {
+		if (currency === "GBP") setSymbol("£");
+		else if (currency === "EUR") setSymbol("€");
+		else if (currency === "USD") setSymbol("$");
+	}, [currency]);
+
+	return (
+		<Crypto.Provider value={{ currency, symbol, setCurrency }}>
+			{children}
+		</Crypto.Provider>
+	);
+};
+
+export default CryptoContext;
+
+export const CryptoState = () => {
+	return useContext(Crypto);
+};
